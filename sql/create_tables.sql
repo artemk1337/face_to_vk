ba20ee91d318b8632008b27e64a2a6c78a9fb84a
Artem K <a.kushch@polymatica.com>
2022-11-13 21:11:07 +0300

    fix

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -31,3 +31,24 @@ CREATE TABLE best_vectors (
     user_id bigint REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE PRIMARY KEY,
     vector_id bigint REFERENCES vectors(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+----------------
+
+-- view users with their best ids
+CREATE VIEW users_with_vectors AS (
+    SELECT u.*, bv.vector_id FROM users u
+    LEFT JOIN best_vectors bv ON u.id = bv.user_id
+);
+
+-- drop finished rows
+CREATE OR REPLACE FUNCTION clean_queue() RETURNS trigger AS $queue_cleaner$
+BEGIN
+    DELETE FROM queue q WHERE q.status = 'finish';
+    RETURN NEW;
+END;
+$queue_cleaner$ LANGUAGE plpgsql;
+
+CREATE TRIGGER queue_cleaner
+AFTER UPDATE ON queue
+FOR EACH ROW
+EXECUTE PROCEDURE clean_queue();
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,4 +1,6 @@
-DROP TABLE queue CASCADE;
+DROP VIEW users_with_vectors;
+
 DROP TABLE best_vectors CASCADE;
 DROP TABLE vectors CASCADE;
 DROP TABLE users CASCADE;
+DROP TABLE queue CASCADE;
